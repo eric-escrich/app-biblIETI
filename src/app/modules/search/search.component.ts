825e6e27b042330208962dcb4617620049ecba0f
cl6mh7 <claudiamoyamo@gmail.com>
2024-04-26 18:15:24 +0200

    chore(acordeon): Styles finished

--- src/app/modules/search/search.component.ts
+++ src/app/modules/search/search.component.ts
@@ -72,7 +72,7 @@ export class SearchComponent implements OnInit {
         { image: 'https://museusdesitges.cat/sites/default/files/styles/img700/public/lalegria_que_passa_de_s._rusinol_30.771.jpg?itok=X2FoltJg' },
         { image: 'https://upload.wikimedia.org/wikipedia/commons/f/f8/Visions_%26_Cants_%281900%29_%28page_3_crop%29.jpg' },
         { image: 'https://imagessl9.casadellibro.com/a/l/s7/89/9788473292689.webp' },
-        { image: 'https://libreria-alzofora.com/wp-content/uploads/2022/06/LISISTRATA-146209.jpg' },
+        { image: 'https://planetalibro.net/biblioteca/e/s/esquilo/esquilo-los-siete-contra-tebas/esquilo-los-siete-contra-tebas.webp' },
         { image: 'https://static.bookscovers.es/imagenes/9789500/978950039871.JPG' },
         { image: 'https://pictures.abebooks.com/inventory/30813148918.jpg' },
         { image: 'https://libreria-alzofora.com/wp-content/uploads/2022/06/LISISTRATA-146209.jpg' },
@@ -85,7 +85,7 @@ export class SearchComponent implements OnInit {
         { image: 'https://libreria-alzofora.com/wp-content/uploads/2022/06/LISISTRATA-146209.jpg' },
         { image: 'https://static.bookscovers.es/imagenes/9789500/978950039871.JPG' },
         { image: 'https://pictures.abebooks.com/inventory/30813148918.jpg' },
-        { image: 'https://libreria-alzofora.com/wp-content/uploads/2022/06/LISISTRATA-146209.jpg' },
+        { image: 'https://m.media-amazon.com/images/I/511ZSAP8PlL.jpg' },
         { image: 'https://www.loqueleo.com/co/uploads/2021/01/la-odisea-1.JPG' },
         { image: 'https://m.media-amazon.com/images/I/81HWYegtSpL._SL1500_.jpg' },
         { image: 'https://upload.wikimedia.org/wikipedia/commons/c/c0/Homer_Ilias_Griphanius_c1572.jpg' },
@@ -93,6 +93,7 @@ export class SearchComponent implements OnInit {
         { image: 'https://upload.wikimedia.org/wikipedia/commons/f/f8/Visions_%26_Cants_%281900%29_%28page_3_crop%29.jpg' },
         { image: 'https://imagessl9.casadellibro.com/a/l/s7/89/9788473292689.webp' }
       ),
+
       this.groupedCities = [
         {
             label: 'Tipus',
