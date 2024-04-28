ffc2465faead22078e0428b15abb3f5778c00b89
Eric Escrich <eric@escrich.net>
2024-04-28 20:55:27 +0200

    Update: Adjusted layout for message functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,22 +8,48 @@ import { SearchComponent } from './modules/search/search.component';
 import { CreacioUsuariComponent } from './modules/creacio-usuari/creacio-usuari.component';
 import { ResetPasswordComponent } from './modules/reset-password-component/reset-password-component.component';
 
+// export const routes: Routes = [
+//     {
+//         path: 'landing',
+//         component: HomeComponent,
+//     },
+//     { path: 'cercar-llibre', component: SearchComponent },
+//     { path: 'itemDetails/:id', component: SearchComponent },
+//     { path: 'reset-password/:uid/:token', component: ResetPasswordComponent },
+//     {
+//         path: '',
+//         component: LayoutComponent,
+//         canActivate: [JwtGuard],
+//         children: [
+//             { path: '', pathMatch: 'full', component: DashboardComponent },
+//             { path: 'creacio-usuari', component: CreacioUsuariComponent },
+//         ],
+//     },
+//     { path: '**', redirectTo: '' },
+// ];
+
 export const routes: Routes = [
-    {
-        path: 'landing',
-        component: HomeComponent,
-    },
-    { path: 'cercar-llibre', component: SearchComponent },
-    { path: 'itemDetails/:id', component: SearchComponent },
-    { path: 'reset-password/:uid/:token', component: ResetPasswordComponent },
     {
         path: '',
         component: LayoutComponent,
-        canActivate: [JwtGuard],
         children: [
-            { path: '', pathMatch: 'full', component: DashboardComponent },
-            { path: 'creacio-usuari', component: CreacioUsuariComponent },
+            {
+                path: 'dashboard',
+                component: DashboardComponent,
+                canActivate: [JwtGuard], // Protege la ruta con el guardia de autenticación
+            },
+            {
+                path: 'creacio-usuari',
+                component: CreacioUsuariComponent,
+                canActivate: [JwtGuard], // Protege la ruta con el guardia de autenticación
+            },
+            { path: 'landing', component: HomeComponent },
+            { path: 'cercar-llibre', component: SearchComponent },
+            { path: 'itemDetails/:id', component: SearchComponent },
+            { path: 'reset-password/:uid/:token', component: ResetPasswordComponent },
         ],
     },
-    { path: '**', redirectTo: '' },
+    { path: '**', redirectTo: 'landing' },
+
+    // Agrega aquí otras rutas que no desciendan de LayoutComponent
 ];
