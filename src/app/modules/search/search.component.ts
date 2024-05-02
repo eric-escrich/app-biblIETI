d0bc4c49da55bca042e2a00aeafa2130308a3a58
cl6mh7 <claudiamoyamo@gmail.com>
2024-05-02 19:04:25 +0200

    chore(search-libros): Changed images for info

--- src/app/modules/search/search.component.ts
+++ src/app/modules/search/search.component.ts
@@ -68,9 +68,6 @@ export class SearchComponent implements OnInit {
             { image: 'https://www.loqueleo.com/co/uploads/2021/01/la-odisea-1.JPG' },
             { image: 'https://m.media-amazon.com/images/I/81HWYegtSpL._SL1500_.jpg' },
             { image: 'https://upload.wikimedia.org/wikipedia/commons/c/c0/Homer_Ilias_Griphanius_c1572.jpg' },
-            {
-                image: 'https://museusdesitges.cat/sites/default/files/styles/img700/public/lalegria_que_passa_de_s._rusinol_30.771.jpg?itok=X2FoltJg',
-            },
             { image: 'https://upload.wikimedia.org/wikipedia/commons/f/f8/Visions_%26_Cants_%281900%29_%28page_3_crop%29.jpg' },
             { image: 'https://imagessl9.casadellibro.com/a/l/s7/89/9788473292689.webp' },
             { image: 'https://planetalibro.net/biblioteca/e/s/esquilo/esquilo-los-siete-contra-tebas/esquilo-los-siete-contra-tebas.webp' },
@@ -80,9 +77,6 @@ export class SearchComponent implements OnInit {
             { image: 'https://www.loqueleo.com/co/uploads/2021/01/la-odisea-1.JPG' },
             { image: 'https://m.media-amazon.com/images/I/81HWYegtSpL._SL1500_.jpg' },
             { image: 'https://upload.wikimedia.org/wikipedia/commons/c/c0/Homer_Ilias_Griphanius_c1572.jpg' },
-            {
-                image: 'https://museusdesitges.cat/sites/default/files/styles/img700/public/lalegria_que_passa_de_s._rusinol_30.771.jpg?itok=X2FoltJg',
-            },
             { image: 'https://upload.wikimedia.org/wikipedia/commons/f/f8/Visions_%26_Cants_%281900%29_%28page_3_crop%29.jpg' },
             { image: 'https://imagessl9.casadellibro.com/a/l/s7/89/9788473292689.webp' },
             { image: 'https://libreria-alzofora.com/wp-content/uploads/2022/06/LISISTRATA-146209.jpg' },
@@ -92,9 +86,6 @@ export class SearchComponent implements OnInit {
             { image: 'https://www.loqueleo.com/co/uploads/2021/01/la-odisea-1.JPG' },
             { image: 'https://m.media-amazon.com/images/I/81HWYegtSpL._SL1500_.jpg' },
             { image: 'https://upload.wikimedia.org/wikipedia/commons/c/c0/Homer_Ilias_Griphanius_c1572.jpg' },
-            {
-                image: 'https://museusdesitges.cat/sites/default/files/styles/img700/public/lalegria_que_passa_de_s._rusinol_30.771.jpg?itok=X2FoltJg',
-            },
             { image: 'https://upload.wikimedia.org/wikipedia/commons/f/f8/Visions_%26_Cants_%281900%29_%28page_3_crop%29.jpg' },
             { image: 'https://imagessl9.casadellibro.com/a/l/s7/89/9788473292689.webp' },
         ),
